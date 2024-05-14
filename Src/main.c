4cc0c4eb7cfb93e733370f0476430b9b83166b34
BIG-PAPA-CODER <117487177+BIG-PAPA-CODER@users.noreply.github.com>
2024-05-14 14:36:36 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -112,13 +112,13 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  BSP_GYRO_Init();
-  BSP_LCD_Init();
-  BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER, LCD_FRAME_BUFFER);
-  BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER, LCD_FRAME_BUFFER);
-  BSP_LCD_DisplayOn();
-  BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
-  BSP_LCD_Clear(LCD_COLOR_WHITE);
+  // BSP_GYRO_Init();
+  // BSP_LCD_Init();
+  // BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER, LCD_FRAME_BUFFER);
+  // BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER, LCD_FRAME_BUFFER);
+  // BSP_LCD_DisplayOn();
+  // BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
+  // BSP_LCD_Clear(LCD_COLOR_WHITE);
   /* USER CODE END Init */
 
   /* Configure the system clock */
